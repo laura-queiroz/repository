1a1ce8f7b8f39c06ee6a9652ed2142edc13c8117
Laura Sa <laura.sa@youse.com.br>
2023-03-13 10:34:07 -0300

    Entity Client created

--- src/application/App.java
+++ src/application/App.java
@@ -1,8 +1,13 @@
 package application;
 
+import application.entities.Client;
+
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        Client c = new Client(1, "Ana Clara");
+        System.out.println(c);
     }
 }
